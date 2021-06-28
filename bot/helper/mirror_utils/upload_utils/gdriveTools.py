0264786faa3e1437d8872b39d4c559037a9d7316
git-hub-lab <64653511+git-hub-lab@users.noreply.github.com>
2021-06-28 17:07:51 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -112,7 +112,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded By V9',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -166,7 +166,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded By V9',
             'mimeType': mime_type,
         }
         try:
